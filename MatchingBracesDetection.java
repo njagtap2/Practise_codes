517dd20f75de2602a080fc6f767aadc923dbd4be
Neha Lalaso Jagtap <neha.jagtap8800@gmail.com>
2016-11-20 18:28:54 -0500

    Update and rename Matching_Braces_Detection.java to MatchingBracesDetection.java

--- MatchingBracesDetection.java
+++ MatchingBracesDetection.java
@@ -1,7 +1,10 @@
+/*Given an expression string exp, write a program to examine whether the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp. For example,
+the program should print true for exp = “[()]{}{[()()]()}” and false for exp = “[(])”*/
+
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Matching_Braces_Detection {
+public class MatchingBracesDetection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
