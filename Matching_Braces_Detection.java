e0dfe93e3f90740d0ac107be65a883e708e7e037
njagtap2 <neha.jagtap8800@gmail.com>
2016-11-20 02:22:50 -0500

    Add files via upload
    For any given input string, check if that all the braces are correctly closed or not.
     eg. vf{v12(vcv}{vc}), this a example of perfectly closing braces. While eg. vf{v12(vcv}{vc(} is not.

---
+++ Matching_Braces_Detection.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Matching_Braces_Detection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter the Input: ");
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		Boolean popOutput = false;
+		char[] ClosingBraces = new char[input.length()];
+
+		Stack myStack = new Stack();
+		char[] stringChars = input.toCharArray();
+		//push the starting braces in to the stack and pop it if it is closing braces. 
+		//If popped braces is not matching one, return false.
+		
+		for (char each : stringChars) {
+			if (each == '[' || each == '(' || each == '{') {
+				myStack.push(each);
+			}
+
+			if (each == ']') {
+				if (myStack.isEmpty() == true) {
+					break;
+				}
+				else if ((char) myStack.pop() == '[')
+					popOutput = true;
+			}
+
+			else if (each == '}') {
+				if (myStack.isEmpty() == true) {
+					break;
+				} else if ((char) myStack.pop() == '{')
+					popOutput = true;
+			}
+
+			else if (each == ')') {
+				if (myStack.isEmpty() == true)
+					break;
+				else if ((char) myStack.pop() == '(')
+					popOutput = true;
+			}
+		}
+		if (popOutput == true) {
+			System.out.println("Balanced string");
+		} else
+			System.out.println("Not balanced tree");
+	}
+
+}
