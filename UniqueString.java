1aa76cab4bf587eed713b4a4f6ca2365c98562bd
njagtap2 <neha.jagtap8800@gmail.com>
2016-11-20 02:27:20 -0500

    Add files via upload
    Check if given string has all unique characters.
    Ex. Neha is a unique string because all the characters in this string are unique.
    But string  "NehaJagtap" is not unique because it has repeating character 'a',

---
+++ UniqueString.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class UniqueString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String input = scan.nextLine();
+		int flag=0;
+		boolean[] setValus = new boolean[126];
+		
+		for(int i=0;i<input.length();i++)
+		{
+			int val = input.charAt(i);
+			if(setValus[val])
+			{
+				flag =1;
+				 break;
+			}
+			setValus[val]=true;
+		}
+			
+		if (flag==1)
+		{
+			System.out.println("String in not unique");
+		}
+		else
+			System.out.println("String is unique");
+		
+	}
+
+}
